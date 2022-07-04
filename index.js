1412e49aab2b4570dc42642c9dbc4eaf66a14236
sharon-odhiambo <88780311+sharon-odhiambo@users.noreply.github.com>
2022-07-04 14:11:08 +0300

    Add form markup

---
+++ index.js
@@ -0,0 +1,14 @@
+const books = [
+    {
+        id: 1,
+        Title: 'Book one',
+        Author: 'Eric',
+    },
+    {
+        id: 2,
+        Title: 'Book two',
+        Author: 'Sharon',
+    }
+]
+
+const Booklist = document.querySelector('#form');
